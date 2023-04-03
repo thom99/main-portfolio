1c2b2822bc3b43a5944a03d3e878c703a3b2c416
thom99 <thomas@damialfredo.com>
2023-04-03 16:43:57 +0200

    fix icon render

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,17 +1,41 @@
+import { useState, useEffect } from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize());
+    }
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((techonlogy) => (
         <div className="w-28 h-28" key={techonlogy.name}>
-          <BallCanvas icon={techonlogy.icon} />
+          {windowSize.innerWidth < 500 && windowSize.innerHeight < 1000 ? (
+            <img src={techonlogy.icon} alt="" />
+          ) : (
+            <BallCanvas icon={techonlogy.icon} />
+          )}
         </div>
       ))}
     </div>
   );
 };
+
+function getWindowSize() {
+  const { innerWidth, innerHeight } = window;
+  return { innerWidth, innerHeight };
+}
 
 export default SectionWrapper(Tech, "");
